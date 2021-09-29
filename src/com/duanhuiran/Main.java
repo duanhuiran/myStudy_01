2e944c0a1f230c44849aa7299281e3d18f554ebc
DuanHuiran <570989207@qq.com>
2021-09-30 00:51:26 +0200

    this is the first task (test)

--- src/com/duanhuiran/Main.java
+++ src/com/duanhuiran/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         int myAge = 23;
-        myAge = 27;
+//        myAge = 27;
         float herAge = myAge;
         System.out.println(herAge);
     }
